03f85cb552251f6d514dae6f7334955a280a01f1
Markus Klink <justjoheinz@gmail.com>
2024-08-27 13:07:01 +0200

    Add test file

---
+++ {{cookiecutter.project_slug}}/t/{{cookiecutter.project_slug}}-tests.lisp
@@ -0,0 +1,7 @@
+(uiop:define-package {{cookiecutter.project_slug}}/tests
+  (:use :cl :parachute))
+
+(in-package :{{cookiecutter.project_slug}}/tests)
+
+
+(test :{{cookiecutter.project_slug}}/tests)
